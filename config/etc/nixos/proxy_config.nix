a4f239a2d7e89b29c0f70b90c48cee6c8e8a9ada
awakum <awakum192@gmail.com>
2025-12-14 13:04:47 +0300

    proxy's hidden

---
+++ config/etc/nixos/proxy_config.nix
@@ -0,0 +1,6 @@
+# rename this file to `.proxy_config.nix` and put the bridge string in the variable
+# The plugin client will be detected automatically
+# Do not forget to `sudo nixos-rebuild switch` after
+{
+  bridge = "obfs4 IP:PORT ..."; # or "snowflake IP:PORT ..."
+}
