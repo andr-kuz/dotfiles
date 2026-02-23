148a6d23aa12ad34b1dc43b486f53ff5495382f2
awakum <awakum192@gmail.com>
2026-02-23 18:04:25 +0300

    feat: kb layout switcher for neovim

--- config/nvim/lua/valtrois/lazy/keyboard_layout.lua
+++ config/nvim/lua/valtrois/lazy/keyboard_layout.lua
@@ -1,7 +1,3 @@
 return {
-  -- 'andr-kuz/xkbswitch.nvim',
-  -- config = function()
-  --   require('xkbswitch').setup()
-  -- --     hyprctl_active_keyboard_name = 'keyd-virtual-keyboard'
-  -- end
+  'ivanesmantovich/xkbswitch.nvim'
 }
