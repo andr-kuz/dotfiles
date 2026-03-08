8b43966b4da6f5faa12b55a864013da41ea12d15
awakum <awakum192@gmail.com>
2026-03-08 15:39:21 +0000

    nvim avante

--- config/nvim/lua/valtrois/lazy/llm.lua
+++ config/nvim/lua/valtrois/lazy/llm.lua
@@ -1,63 +1,69 @@
 return {
-  -- "yetone/avante.nvim",
-  -- -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  -- -- ⚠️ must add this setting! ! !
-  -- build = vim.fn.has("win32") ~= 0
-  --     and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
-  --     or "make",
-  -- event = "VeryLazy",
-  -- version = false, -- Never set this value to "*"! Never!
-  -- ---@module 'avante'
-  -- ---@type avante.Config
-  -- opts = {
-  --   -- add any opts here
-  --   -- this file can contain specific instructions for your project
-  --   instructions_file = "avante.md",
-  --   -- for example
-  --   provider = "ollama",
-  --   providers = {
-  --     ollama = {
-  --       endpoint = "http://localhost:11434", -- Note that there is no /v1 at the end.
-  --       model = "codellama:7b",
-  --     },
-  --   },
-  -- },
-  -- dependencies = {
-  --   "nvim-lua/plenary.nvim",
-  --   "MunifTanjim/nui.nvim",
-  --   --- The below dependencies are optional,
-  --   "nvim-mini/mini.pick", -- for file_selector provider mini.pick
-  --   "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-  --   "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-  --   "ibhagwan/fzf-lua", -- for file_selector provider fzf
-  --   "stevearc/dressing.nvim", -- for input provider dressing
-  --   "folke/snacks.nvim", -- for input provider snacks
-  --   "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-  --   "zbirenbaum/copilot.lua", -- for providers='copilot'
-  --   {
-  --     -- support for image pasting
-  --     "HakonHarnes/img-clip.nvim",
-  --     event = "VeryLazy",
-  --     opts = {
-  --       -- recommended settings
-  --       default = {
-  --         embed_image_as_base64 = false,
-  --         prompt_for_file_name = false,
-  --         drag_and_drop = {
-  --           insert_mode = true,
-  --         },
-  --         -- required for Windows users
-  --         use_absolute_path = true,
-  --       },
-  --     },
-  --   },
-  --   {
-  --     -- Make sure to set this up properly if you have lazy=true
-  --     'MeanderingProgrammer/render-markdown.nvim',
-  --     opts = {
-  --       file_types = { "markdown", "Avante" },
-  --     },
-  --     ft = { "markdown", "Avante" },
-  --   },
-  -- },
+  {
+    "yetone/avante.nvim",
+    build = "make",
+    event = "VeryLazy",
+    version = false,
+    opts = {
+      instructions_file = "avante.md",
+      provider = "routerai",
+      providers = {
+        routerai = {
+          __inherited_from = "openai",
+          endpoint = "https://routerai.ru/api/v1/",
+          model = "qwen/qwen3-coder-plus", -- Модель по умолчанию
+          -- disabled_tools = { "python" }, -- Здесь можно указать какие инструменты Avante не будет использовать
+          model_names = { -- Список моделей доступных для выбора
+            "qwen/qwen3-coder-plus",
+          },
+          api_key_name = 'ROUTERAI_API_KEY',
+          timeout = 30000,
+          extra_request_body = {
+            temperature = 0.75,
+            max_tokens = 16000
+          },
+          extra_headers = {
+            ["HTTP-Referer"] = "https://github.com/yetone/avante.nvim",
+            ["X-Title"] = "Avante.nvim"
+          }
+        },
+      },
+      behaviour = {
+        auto_approve_tool_permissions = false
+      },
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "echasnovski/mini.pick",
+      "nvim-telescope/telescope.nvim",
+      "hrsh7th/nvim-cmp",
+      "ibhagwan/fzf-lua",
+      "stevearc/dressing.nvim",
+      "folke/snacks.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "zbirenbaum/copilot.lua",
+      {
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  }
 }
