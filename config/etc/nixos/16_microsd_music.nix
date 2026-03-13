3d680882eac87c41918f0b4b1a8d84854880a046
awakum <awakum192@gmail.com>
2026-03-13 17:26:40 +0300

    feat: removed automount feature

--- config/etc/nixos/16_microsd_music.nix
+++ config/etc/nixos/16_microsd_music.nix
@@ -4,6 +4,6 @@
   fileSystems."/mnt/16_microsd_music" = {
     device = "/dev/disk/by-uuid/1802-1612";
     fsType = "vfat";
-    options = [ "defaults" "nofail" "x-systemd.automount" ];
+    options = [ "defaults" "nofail" ];
   };
 }
--- config/etc/nixos/tascam_dr_05x_recorder.nix
+++ config/etc/nixos/tascam_dr_05x_recorder.nix
@@ -4,6 +4,6 @@
   fileSystems."/mnt/tascam_dr_05x_recorder" = {
     device = "/dev/disk/by-label/DR-05X";
     fsType = "vfat";
-    options = [ "defaults" "nofail" "x-systemd.automount" ];
+    options = [ "defaults" "nofail" ];
   };
 }
