1974ac6e22620deefd40eaf79e7c753ec97c45e8
awakum <awakum192@gmail.com>
2026-06-03 10:43:03 +0300

    feat: nixos nvim stable

--- home/programs/nvim.nix
+++ home/programs/nvim.nix
@@ -3,28 +3,27 @@
 let
   trigger = builtins.pathExists /var/tmp/nvim.enable;
   neovim-nightly = inputs.neovim-nightly-overlay.packages.${pkgs.system}.default;
+  chosenNeovim = if trigger then neovim-nightly else pkgs.neovim;
 in
 {
-  config = lib.mkIf trigger {
-    home.packages = [
-      neovim-nightly
-      # neovim plugins requirements
-      nodejs
-      yarn
-      gcc
-      clang-tools
-      pyright
-      rustup
-      unzip
-      libxkbfile
-      python3
-      fzf
-      ripgrep
-    ];
+  home.packages = [
+    chosenNeovim
+    
+    # neovim plugins requirements
+    pkgs.nodejs
+    pkgs.yarn
+    pkgs.gcc
+    pkgs.clang-tools
+    pkgs.pyright
+    pkgs.rustup
+    pkgs.unzip
+    pkgs.libxkbfile
+    pkgs.python3
+    pkgs.fzf
+    pkgs.ripgrep
+  ];
 
-    xdg.configFile."nvim" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/config/nvim";
-    };
+  xdg.configFile."nvim" = {
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/config/nvim";
   };
 }
-
