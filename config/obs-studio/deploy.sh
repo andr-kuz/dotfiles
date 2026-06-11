56c5f5b7ae7fc12884cf61c6b3e23dd829083299
awakum <awakum192@gmail.com>
2026-06-11 15:53:11 +0300

    fix: OBS config

--- config/obs-studio/deploy.sh
+++ config/obs-studio/deploy.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
+cd ~/.dotfiles/config/obs-studio/
 mkdir -p ~/.config/obs-studio
-cp -rsv "$(pwd)"/* ~/.config/obs-studio/
+find . -type f | while read -r n; do
+    if [ "$(basename "$n")" != "deploy.sh" ]; then
+        target_dir="$HOME/.config/obs-studio/$(dirname "$n")"
+        mkdir -p "$target_dir"
+        ln -vrs "$n" "$target_dir/"
+    fi
+done
