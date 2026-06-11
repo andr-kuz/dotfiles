3ef613f64aeaae6586f8964ce5229e76a28450ba
awakum <awakum192@gmail.com>
2026-06-11 15:48:37 +0300

    fix: REAPER config

--- config/REAPER/deploy.sh
+++ config/REAPER/deploy.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+cd ~/.dotfiles/config/REAPER/
 mkdir -p ~/.config/REAPER
-for n in ./*; do ln -vs "$n" ~/.config/REAPER; done
+for n in ./*; do 
+    if [ "$(basename "$n")" != "deploy.sh" ]; then
+        ln -vrs "$n" ~/.config/REAPER/
+    fi
+done
