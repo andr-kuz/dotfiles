bde697430edfe427a7242f208190a7cf7ee94483
awakum <awakum192@gmail.com>
2026-06-16 10:36:29 +0300

    feat: nvim scrolloff

--- config/nvim/lua/valtrois/setup.lua
+++ config/nvim/lua/valtrois/setup.lua
@@ -94,3 +94,20 @@ vim.filetype.add({
 })
 
 vim.g.netrw_banner = 0  -- disable file explorer banner
+
+vim.opt.scrolloff = 8 -- how many lines above/below the cursor
+-- the function below keeps scrolloff working even at the end of the file
+vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI", "BufEnter" }, {
+    group = vim.api.nvim_create_augroup("ScrollOffEOF", {}),
+    callback = function()
+        local win_h = vim.api.nvim_win_get_height(0)
+        local off = math.min(vim.o.scrolloff, math.floor(win_h / 2))
+        local dist = vim.fn.line "$" - vim.fn.line "."
+        local rem = vim.fn.line "w$" - vim.fn.line "w0" + 1
+        if dist < off and win_h - rem + dist < off then
+            local view = vim.fn.winsaveview()
+            view.topline = view.topline + off - (win_h - rem + dist)
+            vim.fn.winrestview(view)
+        end
+    end,
+})
