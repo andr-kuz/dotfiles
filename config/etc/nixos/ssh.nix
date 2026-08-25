e616fb060b9c11c23bbe24747f4328953354ca6c
awakum <awakum192@gmail.com>
2026-08-25 21:55:01 +0300

    feat: nixos ssh laptop pub key

--- config/etc/nixos/ssh.nix
+++ config/etc/nixos/ssh.nix
@@ -17,7 +17,8 @@
       "wheel" 
     ];
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFTyRXVHdJ121Vkw6COUGLgReKQu9OF3wGvkHDGa/5NC"  # phone
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFTyRXVHdJ121Vkw6COUGLgReKQu9OF3wGvkHDGa/5NC"  # phone   -> desktop
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII5PvUlyOZqPxR0phqym3ulIgr/eM7WgEXqGXLurC9Gy"  # laptop  -> desktop
     ];
   };
 }
