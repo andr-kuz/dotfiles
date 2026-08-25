4220aa4a315d3593404964e8b9ba741e065df3ab
awakum <awakum192@gmail.com>
2026-08-25 20:31:01 +0300

    feat: nixos kdeconnect

--- config/etc/nixos/kdeconnect.nix
+++ config/etc/nixos/kdeconnect.nix
@@ -2,15 +2,38 @@
 
 let
   trigger = builtins.pathExists /var/tmp/kdeconnect.enable;
+
+  hypr-kdeconnect-fix = pkgs.stdenv.mkDerivation {
+    pname = "hypr-kdeconnect-fix";
+    version = "unstable";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "gfhdhytghd";
+      repo = "hypr-kdeconnect-fix";
+      rev = "master"; # or specific commit hash
+      sha256 = "sha256-VcXxVtlnkPjO6l0ky/n+0qa87Uc3c8hRM0twfgl+AiM="; # replace with actual hash
+    };
+
+    nativeBuildInputs = with pkgs; [
+      cmake
+      ninja
+      pkg-config
+      qt6.wrapQtAppsHook
+    ];
+
+    buildInputs = with pkgs; [
+      qt6.qtbase
+      wayland
+      libxkbcommon
+      libei
+      xdg-desktop-portal
+    ];
+  };
 in
 lib.mkIf trigger {
   programs.kdeconnect.enable = true;
-  environment.systemPackages = with pkgs; [
-    xdg-desktop-portal-hyprland
+  environment.systemPackages = [ 
+    hypr-kdeconnect-fix 
+    pkgs.wl-clipboard
   ];
-
-  # networking.firewall = lib.mkIf trigger (rec {
-  #   allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
-  #   allowedUDPPortRanges = allowedTCPPortRanges;
-  # });
 }
