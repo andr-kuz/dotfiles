912c674abdb8bdefa73c479865fb3af27490cf91
awakum <awakum192@gmail.com>
2026-09-17 21:08:16 +0300

    feat: nixos added ssh key

--- config/etc/nixos/ssh.nix
+++ config/etc/nixos/ssh.nix
@@ -19,6 +19,7 @@
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFTyRXVHdJ121Vkw6COUGLgReKQu9OF3wGvkHDGa/5NC"  # phone   -> desktop
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII5PvUlyOZqPxR0phqym3ulIgr/eM7WgEXqGXLurC9Gy"  # laptop  -> desktop
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIECDWDve49vnG5kGnSFrD0dq+YdJoc+8eFxtXIaEeYMB"  # desktop -> laptop
     ];
   };
 }
