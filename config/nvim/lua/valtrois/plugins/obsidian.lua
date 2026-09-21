dcd55cd3a941382e4acc50b0af4d6789b6b01efc
awakum <awakum192@gmail.com>
2026-09-21 15:21:59 +0300

    feat: nvim updated options for new obsidian plugin

--- config/nvim/lua/valtrois/plugins/obsidian.lua
+++ config/nvim/lua/valtrois/plugins/obsidian.lua
@@ -27,17 +27,19 @@ return {
       return tostring(title)
     end,
       -- Optional, alternatively you can customize the frontmatter data.
-    note_frontmatter_func = function(note)
-      -- This is equivalent to the default frontmatter function.
-      local out = { aliases = note.aliases, tags = note.tags }
-      -- `note.metadata` contains any manually added fields in the frontmatter.
-      -- So here we just make sure those fields are kept in the frontmatter.
-      if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
-        for k, v in pairs(note.metadata) do
-          out[k] = v
+    frontmatter = {
+      func = function(note)
+        -- This is equivalent to the default frontmatter function.
+        local out = { aliases = note.aliases, tags = note.tags }
+        -- `note.metadata` contains any manually added fields in the frontmatter.
+        -- So here we just make sure those fields are kept in the frontmatter.
+        if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+          for k, v in pairs(note.metadata) do
+            out[k] = v
+          end
         end
-      end
-      return out
-    end,
+        return out
+      end,
+    },
   },
 }
